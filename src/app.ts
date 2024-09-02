edb5146efc81b7c2f3e1bb21964c69c710b9ab91
andrew <andrewgeeklab@gmail.com>
2024-09-02 14:36:54 +0300

    remove checkAuth from content-creation

--- src/app.ts
+++ src/app.ts
@@ -44,9 +44,9 @@ app.get('/check-tld', async (_, res) => {
 app.use(`/${RouterEnum.authentication}`, AuthenticationRouter);
 app.use(`/${RouterEnum.unAuthorizer}`, unAuthorizerApis)
 app.use(`/${RouterEnum.calendly}`, CalendlyRouter);
+app.use(`/${RouterEnum.ContentCreation}`, content_creation_router)
 app.use(checkAuthority)
 app.use(`/${RouterEnum.hr}`, HR_Router);
 app.use(`/${RouterEnum.admin}`, AdminRouter);
 app.use(`/${RouterEnum.user}`, UserRouter);
-app.use(`/${RouterEnum.ContentCreation}`, content_creation_router)
 export { app };
