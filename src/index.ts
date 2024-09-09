cd29031e72e6ac809f2505b753aa79162dd53ee9
abdo <abdulrahmangeeklab1@gmail.com>
2024-09-09 09:43:22 +0300

    some changes

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,11 @@ import "dotenv/config";
 import { DBConnection } from "./DbSetup/DbConfig";
 import ScrapCronJob from "./Utils/CronJobs/ScrapCronJob";
 import createIo from "./socketIo";
-import { ConvertAudio } from "./Controller/VideoEditing/ConvertAudio_tts";
 
 const port = process.env.PORT || 4000;
 const server = app.listen(port, async () => {
   try {
     await DBConnection();
-    await ConvertAudio("Today is a wonderful day to build something people love!")
     console.log(`Server is Running And DB Connected http://localhost:${port}`);
     ScrapCronJob.start();
   } catch (error) {
