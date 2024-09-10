600a9e0fc31466e3c6b5cd2aee8a857b793602e8
abdo <abdulrahmangeeklab1@gmail.com>
2024-09-10 15:04:56 +0300

    some changes

--- src/middleware/socket.auth.js
+++ src/middleware/socket.auth.js
@@ -4,13 +4,15 @@ import authenticationService from "../Service/Authentication/AuthenticationServi
 export const isAuthenticated = async (socket, next) => {
   try {
     console.log("from authentication: ", socket.id);
-    let token = socket.handshake.auth.token;
+    let token =
+      socket.handshake.auth.token || socket.handshake.headers.authorization;
+    console.log({ token });
     if (!token) return next(new Error("Token is required!"));
 
     token = token.split(" ")[1];
     const decoded = await authenticationService.verifyToken(token);
     const user = await employeeModel.findById(decoded._id);
-
+    console.log({ user });
     if (!user) return next(new Error("User not found!"));
 
     socket.handshake.user = user;
