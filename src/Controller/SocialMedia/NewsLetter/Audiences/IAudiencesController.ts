0c18579fe8f2df72c6a9c46bc76cd4cd92b26f7e
AnDrEwGeEkLaB <andrewgeeklab@gmail.com>
2024-09-23 16:22:41 +0300

    add audience emails (#115)
    Co-authored-by: GeekLabsHoldings <129297744+GeekLabsHoldings@users.noreply.github.com>

--- src/Controller/SocialMedia/NewsLetter/Audiences/IAudiencesController.ts
+++ src/Controller/SocialMedia/NewsLetter/Audiences/IAudiencesController.ts
@@ -7,9 +7,18 @@ export interface IAudienceResponse {
         UnSubscribers: { this_month: number, last_month: number }
     }
 }
+
+export interface IAudienceAnalysisResponse {
+    email: string,
+    contactRating: number,
+    brand: string,
+    subscription: boolean,
+    createdAt: number
+}
 export default interface IAudienceController {
     addNewUser(email: string, brand: string): Promise<void>;
     unSubscribeUser(email: string, brand: string): Promise<void>;
     getAllUsers(brand: string): Promise<string[]>;
     getAudiencesAnalysis(brand: string): Promise<IAudienceResponse>;
+    getAudiencesEmails(brand: string, queryType: string): Promise<IAudienceAnalysisResponse[]>;
 }
--- src/Service/NewsLetter/UserSubscription/IUserSubscriptionService.ts
+++ src/Service/NewsLetter/UserSubscription/IUserSubscriptionService.ts
@@ -6,4 +6,6 @@ export default interface IUserSubscriptionService {
     countUsersSubscriptionByBrand(brand: string): Promise<number>;
     unSubscribeUser(email: string, brand: string, updatedAt: number): Promise<void>;
     countUsersByBrandAndDate(brand: string, startDate: number): Promise<any>;
+    addReceivedEmails(email: string, brand: string): Promise<void>;
+    getUsersReceivedEmails(brand: string): Promise<IUserSubscriptionModel[]>;
 }
