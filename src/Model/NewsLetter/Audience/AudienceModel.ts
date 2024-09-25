23f8df8ea99551cb3315efdf522fc3c700710ec3
GeekLabsHoldings <129297744+GeekLabsHoldings@users.noreply.github.com>
2024-09-25 16:42:48 +0300

    change to specific number

--- src/Model/NewsLetter/Audience/AudienceModel.ts
+++ src/Model/NewsLetter/Audience/AudienceModel.ts
@@ -1,12 +1,12 @@
 import { model, Schema } from 'mongoose';
 import IAudienceModel from './IAudienceModel';
-import { RequiredNumber, RequiredString } from '../../../Utils/Schemas';
+import { RequiredNumber, RequiredSpecificNumber, RequiredString } from '../../../Utils/Schemas';
 import { SchemaTypesReference } from '../../../Utils/Schemas/SchemaTypesReference';
 
 const AudienceSchema = new Schema<IAudienceModel>({
     brand: RequiredString,
     date: RequiredNumber,
-    count: RequiredNumber
+    count: RequiredSpecificNumber(1)
 });
 
 
