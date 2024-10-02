210e40de6ce298f2ef8348d3e47b2b741574a1b9
unknown <mohamedmamdouhgeeklab@gmail.com>
2024-10-02 17:00:17 +0300

    add new img API

--- src/Controller/VideoEditing/fetchImages.js
+++ src/Controller/VideoEditing/fetchImages.js
@@ -19,6 +19,26 @@ const getImg = async (req, res) => {
     }
 };
 
+const getImgNew = async (req, res) => {
+  try {
+    const { searchImgKeyword } = req.body;
+    if (!searchImgKeyword) {
+      return res
+        .status(400)
+        .json({ success: false, error: "No image name provided" });
+    }
+    const images = await servicesGetImages.handleSearchImgNew(searchImgKeyword);
+
+    return res.json({ success: true, images });
+  } catch (error) {
+    console.error("Error in getImg:", error);
+    return res
+      .status(500)
+      .json({ success: false, error: "Something went wrong!" });
+  }
+};
+
 module.exports = {
-  getImg
+  getImg,
+  getImgNew
 }
