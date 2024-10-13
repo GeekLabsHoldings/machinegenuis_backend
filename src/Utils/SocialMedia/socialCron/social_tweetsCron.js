b966c0941e2e6bcd33b223afd9f7f113b237801f
abdo <abdulrahmangeeklab1@gmail.com>
2024-10-13 09:31:09 +0300

    edit social media

--- src/Utils/SocialMedia/socialCron/social_tweetsCron.js
+++ src/Utils/SocialMedia/socialCron/social_tweetsCron.js
@@ -2,6 +2,10 @@ import cron from "node-cron";
 import { tweets } from "../../../Controller/SocialMedia/get_tweets_mustApprove";
 export const twitterCron = cron.schedule("*/30 * * * *", async () => {
   try {
+    if (process.env.NODE_ENV !== 'PROD') {
+      console.log("Cron job skipped in non-production environment.");
+      return;
+  }
     const response = await tweets();
   } catch (error) {
     console.log(error);
