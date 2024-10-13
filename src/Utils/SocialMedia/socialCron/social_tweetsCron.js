6b514f57276cb5a21a6bd63ea3f2e00ba29fbdb8
abdo <abdulrahmangeeklab1@gmail.com>
2024-10-13 14:34:42 +0300

    add image url account

--- src/Utils/SocialMedia/socialCron/social_tweetsCron.js
+++ src/Utils/SocialMedia/socialCron/social_tweetsCron.js
@@ -2,6 +2,10 @@ import cron from "node-cron";
 import { tweets } from "../../../Controller/SocialMedia/get_tweets_mustApprove";
 let isRunning = false;
 export const twitterCron = cron.schedule("*/30 * * * *", async () => {
+  if (process.env.REDIS_PORT !== "16388") {
+    console.log("Cron job skipped in non-production environment.");
+    return;
+}
   if (isRunning) {
     console.log("Job is already running. Skipping this execution.");
     return;
@@ -9,10 +13,6 @@ export const twitterCron = cron.schedule("*/30 * * * *", async () => {
   isRunning = true;
 
   try {
-    if (process.env.REDIS_PORT !== "16388") {
-      console.log("Cron job skipped in non-production environment.");
-      return;
-  }
     const response = await tweets();
   } catch (error) {
     console.log(error);
