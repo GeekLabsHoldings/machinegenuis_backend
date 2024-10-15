bfa4ccb081544ed421ae7537604898ea62b8e13f
GeekLabsHoldings <129297744+GeekLabsHoldings@users.noreply.github.com>
2024-10-15 13:15:19 +0300

    changes

--- src/Service/HR/LinkedinAccounts/LinkedinAccountService.ts
+++ src/Service/HR/LinkedinAccounts/LinkedinAccountService.ts
@@ -1,10 +1,9 @@
 import { Types } from "mongoose";
-import ILinkedinAccountCookiesModel from "../../../Model/HR/LinkedinAccounts/ILinkedinAccountCookiesModel";
 import LinkedinAccountCookiesModel from "../../../Model/HR/LinkedinAccounts/LinkedinAccountCookiesModel";
-import ILinkedinAccountsService from "./ILinkedinAccountsService";
+import ILinkedinAccountsService, { ILinkedinAccountCookiesModel } from "./ILinkedinAccountsService";
 
 export default class LinkedinAccountService implements ILinkedinAccountsService {
-    public async getBusyAccounts(): Promise<(ILinkedinAccountCookiesModel & { _id: Types.ObjectId | string })[]> {
+    public async getBusyAccounts(): Promise<ILinkedinAccountCookiesModel[]> {
         return await LinkedinAccountCookiesModel.find({ isBusy: true });
     }
 }
