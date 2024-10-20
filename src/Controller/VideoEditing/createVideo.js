0366a796058523f5b4fef4d5d174983e479ede72
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2024-10-20 13:39:02 +0300

    some changes

--- src/Controller/VideoEditing/createVideo.js
+++ src/Controller/VideoEditing/createVideo.js
@@ -1,7 +1,6 @@
 import { findYouTubeLinksForKeywords } from "../../Service/VideoEditingModule/searchInYoutube.js";
 import {
   splitContentInvestocracy,
-  generateIntroKeyword,
   generateIntroJson,
 } from "../../Service/VideoEditingModule/splitContent";
 export const gitDetailsContent = async (req, res) => {
@@ -24,68 +23,3 @@ export const gitDetailsContent = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error", details: error.message });
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export const gitDetailsContent = async (req, res) => {
-//   try {
-//     const { intro, content } = req.body;
-//     const introGenerate = await generateIntroJson(intro);
-//     const bodyAndOutro = await splitContentInvestocracy(content);
-
-//     console.log("bodyAndOutro", bodyAndOutro);
-//     console.log("introGenerate", introGenerate);
-
-//     const introKeywords = introGenerate.keywordsAndImages.map(item => item.keyword);
-//     const videoLinks = await findYouTubeLinksForKeywords(introKeywords, bodyAndOutro);
-
-
-//     videoLinks.intro.text = introGenerate.text;
-//     videoLinks.intro.keywords = introKeywords;
-//     videoLinks.intro.audioPath = introGenerate.audioPath;
-
-//     console.log("videoLinks----", videoLinks);
-//     return res.status(200).json(videoLinks);
-//   } catch (error) {
-//     console.error("Error generating recap:", error.message, error.stack);
-//     res.status(500).json({ error: "Internal Server Error", details: error.message });
-//   }
-// };
-
-// export const gitDetailsContent = async (req, res) => {
-//   try {
-//     const { intro, content } = req.body;
-//     const introGenerate = await generateIntroJson(intro);
-//     const bodyAndOutro = await splitContentInvestocracy(content);
-
-//     console.log("bodyAndOutro", bodyAndOutro);
-//     console.log("introGenerate", introGenerate);
-
-//     const keywords = bodyAndOutro.map((item) => item.keywords).flat();
-//     const outro = bodyAndOutro.map((item) => item.outro).flat();
-//     const videoLinks = await findYouTubeLinksForKeywords(keywords, bodyAndOutro, introGenerate);
-
-//     console.log("videoLinks----", videoLinks);
-//     return res.status(200).json(videoLinks);
-//   } catch (error) {
-//     console.error("Error generating recap:", error.message, error.stack);
-//     res.status(500).json({ error: "Internal Server Error", details: error.message });
-//   }
-// };
