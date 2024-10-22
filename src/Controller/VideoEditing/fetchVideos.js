ae23dee3e1a725676f260e8411948bb421aa4519
abdo <abdulrahmangeeklab1@gmail.com>
2024-10-22 11:14:11 +0300

    edit video inv

--- src/Controller/VideoEditing/fetchVideos.js
+++ src/Controller/VideoEditing/fetchVideos.js
@@ -1,4 +1,4 @@
-const { searchVideosYouTube } = require("../../Service/VideoEditingModule/searchInYoutube");
+const { searchVideosYouTube, searchVideosYouTubeCnbc } = require("../../Service/VideoEditingModule/searchInYoutube");
 
 const searchVideosOnYouTube = async (req, res) => {
     try {
@@ -13,6 +13,20 @@ const searchVideosOnYouTube = async (req, res) => {
         .json({ error: "Internal Server Error", details: error.message });
     }
   };
+  const searchVideosOnYouTubeCnbc = async (req, res) => {
+    try {
+      const { query } = req.body;
+      const videos = await searchVideosYouTubeCnbc(query);
+      
+      res.status(200).json(videos);
+    } catch (error) {
+      console.error("Error fetching videos:", error.message, error.stack);
+      res
+        .status(500)
+        .json({ error: "Internal Server Error", details: error.message });
+    }
+  };
   module.exports = {
     searchVideosOnYouTube,
+    searchVideosOnYouTubeCnbc
   };
